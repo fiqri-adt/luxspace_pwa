b45d5adc99fc97c5746302ffe37caa96446a0bb8
fiqri aditya <72197739+fiqri-adt@users.noreply.github.com>
2022-04-10 06:20:11 +0700

    landing
    Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "./component/Header";
 import Hero from "./component/Hero";
+import Browse from "./component/Browse";
+import Arrived from "./component/Arrived";
+import Client from "./component/Client";
+import AsideMenu from "./component/AsideMenu";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Browse />
+      <Arrived />
+      <Client />
+      <AsideMenu />
     </>
   );
 }
